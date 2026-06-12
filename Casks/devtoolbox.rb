4f82162c284b8b63b4de53415150233a831a554d
Vuong <3168632+vuon9@users.noreply.github.com>
2026-06-12 11:26:40 +0700

    Add DevToolbox cask

---
+++ Casks/devtoolbox.rb
@@ -0,0 +1,11 @@
+cask "devtoolbox" do
+  version "0.10.0-rc.2"
+  sha256 "60a0848e6699af69db375bdac8cf7a1376085415c8c6dca78fe79bf18b4763dd"
+
+  url "https://github.com/vuon9/devtoolbox/releases/download/v#{version}/DevToolbox-macos-universal.dmg"
+  name "DevToolbox"
+  desc "Essential dev tools for daily tasks"
+  homepage "https://github.com/vuon9/devtoolbox"
+
+  app "DevToolbox.app"
+end
---
+++ README.md
@@ -0,0 +1,9 @@
+# homebrew-tap
+
+Homebrew tap for vuon9 software.
+
+## Install DevToolbox
+
+```sh
+brew install --cask vuon9/tap/devtoolbox
+```
